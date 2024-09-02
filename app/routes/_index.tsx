a3124ff2017d81f48919a6069571aa5a891e68d9
Terence Faid JABO <faidterence@outlook.com>
2024-09-02 12:30:23 +0200

    feat: integrate dictionary API and search functionality into Landing Page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,7 +4,7 @@ import { Button } from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Word Flow" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
